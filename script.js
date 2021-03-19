def132d431128add86819bc33d69462c5956fc8f
Sean Kelly <invertedrdm@gmail.com>
2021-03-18 23:56:03 -0400

    reseting fetch syntax to see if anything changes

--- script.js
+++ script.js
@@ -4,15 +4,17 @@
 var testUrl = 'https://www.mapquestapi.com/search/v4/place?location=-74.95590458465354%2C40.26624146333869&sort=relevance&feedback=false&key=9UthBdDGZK1MsiEFy48XWw3fWtC01AAJ&pageSize=5&q=parks'
 
 
-  fetch(testUrl, {
-  method: 'GET', //GET is the default.
-  credentials: 'same-origin', // include, *same-origin, omit
-  redirect: 'follow', // manual, *follow, error
-})
-  .then(function (response) {
-      console.log(response);
-    return response;
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+  // fetch(testUrl)
+  // .then(function (response) {
+  //   return response;
+  // })
+  // .then(function (data) {
+  //   console.log(data);
+  // });
+
+  // GET Request.
+fetch(testUrl)
+// Handle success
+.then(response => response.json())  // convert to json
+.then(json => console.log(json))    //print data to console
+.catch(err => console.log('Request Failed', err)); // Catch errors
