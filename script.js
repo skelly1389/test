8e7e0115b17c7c76f19c9321116caf003f241c34
Sean Kelly <invertedrdm@gmail.com>
2021-03-19 06:46:55 -0400

    added comments, more typo

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-// mapquest key
-// mtbhj6FHUDK65jhm5YNhCClvB7GI52JS
-
+// known working api urls for testing functions
 var testSearchUrl = 'https://www.mapquestapi.com/search/v4/place?location=-74.95590458465354%2C40.26624146333869&sort=relevance&feedback=false&key=9UthBdDGZK1MsiEFy48XWw3fWtC01AAJ&pageSize=5&q=parks'
 var testLocationUrl = 'https://www.mapquestapi.com/geocoding/v1/address?key=mtbhj6FHUDK65jhm5YNhCClvB7GI52JS&location=philadelphia,pa';
+// can be set as user input once we get everything connected
 var testInput = 'Philadelphia, PA';
+// some dude on stackoverflow says this is how to remove spaces, seems to work
 var testLocation = testInput.replace(/\s+/g, '');
 
+// uses mapquest api to get coordinates based on city, state or zip
 function getLocation(){
   fetch('https://www.mapquestapi.com/geocoding/v1/address?key=mtbhj6FHUDK65jhm5YNhCClvB7GI52JS&location=' + testLocation)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
+    //grabs the coordinates from the api call
     var userLon = data.results[0].locations[0].latLng.lng;
     var userLat = data.results[0].locations[0].latLng.lat;
+    //plugs the coords into mapquest search places api
     getParks(userLon, userLat)
   })
   }
 
+//searches for 5 parks near coords, sorts by relevance for now because the filter is a query and not built in
 function getParks(lon, lat){
   fetch('https://www.mapquestapi.com/search/v4/place?location=' + lon + '%2C' + lat +'&sort=relevance&feedback=false&key=9UthBdDGZK1MsiEFy48XWw3fWtC01AAJ&pageSize=5&q=parks')
   .then(function (response) {
@@ -25,10 +29,12 @@ function getParks(lon, lat){
   })
   .then(function (data) {
     console.log(data);
-    for(i=0;i<5;i++){
+    //console logging info to make it easier to figure out what we'll want printed to the page
+    for(i=0;i<data.results.length;i++){
       console.log('park name: ' + data.results[i].name);
     }
   })
   }
 
+//can set up an event listener for click, just running on page load to test
 getLocation();
