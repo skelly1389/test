5b9304a11649f118f901c1a336034f63c6701b0f
Sean Kelly <invertedrdm@gmail.com>
2021-03-19 05:14:33 -0400

    forgot the s

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // mtbhj6FHUDK65jhm5YNhCClvB7GI52JS
 
 var testSearchUrl = 'https://www.mapquestapi.com/search/v4/place?location=-74.95590458465354%2C40.26624146333869&sort=relevance&feedback=false&key=9UthBdDGZK1MsiEFy48XWw3fWtC01AAJ&pageSize=5&q=parks'
-var testLocationUrl = 'http://www.mapquestapi.com/geocoding/v1/address?key=mtbhj6FHUDK65jhm5YNhCClvB7GI52JS&location=philadelphia,pa';
+var testLocationUrl = 'https://www.mapquestapi.com/geocoding/v1/address?key=mtbhj6FHUDK65jhm5YNhCClvB7GI52JS&location=philadelphia,pa';
 var testLocation = 'Philadelphia, PA';
 
 function getLocation(){
