0878a86197c44ca11cfedc07b8527e54ba27c238
Sean Kelly <invertedrdm@gmail.com>
2021-03-19 06:26:13 -0400

    testing nested functions for calls

--- script.js
+++ script.js
@@ -3,20 +3,29 @@
 
 var testSearchUrl = 'https://www.mapquestapi.com/search/v4/place?location=-74.95590458465354%2C40.26624146333869&sort=relevance&feedback=false&key=9UthBdDGZK1MsiEFy48XWw3fWtC01AAJ&pageSize=5&q=parks'
 var testLocationUrl = 'https://www.mapquestapi.com/geocoding/v1/address?key=mtbhj6FHUDK65jhm5YNhCClvB7GI52JS&location=philadelphia,pa';
-var testLocation = 'Philadelphia, PA';
+var testInput = 'Philadelphia, PA';
+var testLocation = testInput.replace(/\s+/g, '');
 
 function getLocation(){
-  fetch(testLocationUrl)
-  .then(response => response.json())
-  .then(json => console.log(json))
-  .catch(err => console.log('Request Failed', err));
-}
+  fetch('https://www.mapquestapi.com/geocoding/v1/address?key=mtbhj6FHUDK65jhm5YNhCClvB7GI52JS&location=' + testLocation)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    var userLon = data.results[0].locations[0].latLng.lng;
+    var userLat = data.results[0].locations[0].latLng.lat;
+    getParks(userLon, userLat)
+  })
+  }
 
-function getParks(){
-  fetch(testSearchUrl)
+function getParks(lon, lat){
+  console.log('test');
+  fetch('https://www.mapquestapi.com/search/v4/place?location=' + lon + '%2C' + lat +'&sort=relevance&feedback=false&key=9UthBdDGZK1MsiEFy48XWw3fWtC01AAJ&pageSize=5&q=parks')
   .then(response => response.json())
   .then(json => console.log(json))
-  .catch(err => console.log('Request Failed', err));
 }
 
 getLocation();
+// getParks();
+
