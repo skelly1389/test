96409c31c8cc683d05e6dfcc577a83b72bdeb9d4
Sean Kelly <invertedrdm@gmail.com>
2021-03-18 23:35:14 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// mapquest key
+// mtbhj6FHUDK65jhm5YNhCClvB7GI52JS
+
+var testUrl = 'https://www.mapquestapi.com/search/v4/place?location=-74.95590458465354%2C40.26624146333869&sort=relevance&feedback=false&key=9UthBdDGZK1MsiEFy48XWw3fWtC01AAJ&pageSize=5&q=parks'
+
+
+  fetch(testUrl, {
+  method: 'GET', //GET is the default.
+  credentials: 'same-origin', // include, *same-origin, omit
+  mode: 'cors',
+  redirect: 'follow', // manual, *follow, error
+})
+  .then(function (response) {
+      console.log(response);
+    return response.json;
+  })
+  .then(function (data) {
+    console.log(data);
+  });
